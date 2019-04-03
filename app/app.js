08463ff58da4867c02c84fce32caaff18129e165
Hunter Mason <hunternmason@gmail.com>
2019-04-03 16:33:46 -0500

    Started integration with recording library

--- app/app.js
+++ app/app.js
@@ -1,3 +1,17 @@
+URL = window.URL 
+
+var gumStream;                   //stream from getUserMedia()
+var rec;                      //Recorder.js object
+var input;                    //MediaStreamAudioSourceNode we'll be recording
+
+// shim for AudioContext when it's not avb. 
+var AudioContext = window.AudioContext 
+var audioContext //audio context to help us record
+
+var recordButton = document.getElementById("recordButton");
+var stopButton = document.getElementById("stopButton");
+var pauseButton = document.getElementById("pauseButton");
+
 var loadLocalStorage = function () {
 	var keys = Object.keys(localStorage)
 	var htmlString = '';
@@ -10,13 +24,14 @@ var loadLocalStorage = function () {
 var updateStatusLabel = function(message) {
 	$('#statusLabel').text('Status: ' + message);
 }
-
  //jQuery document ready initialization stuff
  ////button and form event handlers
  // logic for determining action probably needs to go in the event handler
 $(document).ready(function () {
 	loadLocalStorage();
+	// sets the defaul sound library to be piano
 	var instrument = 'piano';
+	// gives user option to choose between two distinct sound libraries
 	$('#btn-808').on('click', function(e) {
 			instrument = '808'
 			console.log(instrument)
@@ -28,7 +43,8 @@ $(document).ready(function () {
 			console.log(instrument)
 			return instrument;
 	});
-
+	// logic for determining which key is pressed, and which sound to play
+	// changes visual representation of keyboard on keydown and keyup
 	$(document).on('keydown', function(e) {
     if (e.which === 65) {
     	$("#C2-Key").prop('src', 'file:///Users/huntermason/Desktop/C%20Key%20copy%202.jpg')
@@ -38,7 +54,6 @@ $(document).ready(function () {
 			var audioA = new Audio('file:///Users/huntermason/Documents/Long%20Piano%20Samples/Piano%20C2.wav')
 		}
 		audioA.play();
-
 		$(document).on('keyup', function() {
 			$("#C2-Key").prop('src', 'file:///Users/huntermason/Desktop/C%20Key.jpg')
 		});
@@ -177,7 +192,8 @@ $(document).ready(function () {
     }
 	});
 
-	$('#btn-create').on('click', function(e) {
+	$('#recordButton').on('click', function(e) {
+		startRecording();
 		var key = $('#key').val();
 		var value = $('#value').val();
 		var keyExists = localStorage.getItem(key) !== null;
@@ -194,17 +210,43 @@ $(document).ready(function () {
 		loadLocalStorage();
 	});
 
+	$('#pauseButton').on('click', function(e) {
+		pauseRecording();
+	});
 
+	$('#stopButton').on('click', function(e) {
+		stopRecording();
+	});
+	// $('#btn-create').on('click', function(e) {
+	// 	var key = $('#key').val();
+	// 	var value = $('#value').val();
+	// 	var keyExists = localStorage.getItem(key) !== null;
+
+	// 	if (keyExists) {
+	// 		updateStatusLabel('key already exists, please use update button instead! :D');
+	// 	} else if (key === '') {
+	// 		updateStatusLabel('invalid input!')
+	// 	}else {
+	// 		createEntry(key, value);
+	// 		updateStatusLabel('key created - ' + key);
+	// 	}
+
+	// 	loadLocalStorage();
+	// });
 	$('#btn-update').on('click', function(e) {
 		var key = $('#key').val();
 		var value = $('#value').val();
 		var existingValue = localStorage.getItem(key)
 		var keyExists = existingValue !== null;
+		var file = '#' + key;
 		if (value === existingValue) {
 			updateStatusLabel('key not updated - that value already exists silly! xD')
 		} else if (keyExists) {
 			updateEntry(key, value);
+			$(file).remove();
+			startRecording();
 			updateStatusLabel('key updated - ' + key);
+
 		} else if (key === '') {
 			updateStatusLabel('invalid input!')
 		} else {
@@ -218,9 +260,11 @@ $(document).ready(function () {
 		var key = $('#key').val();
 		var value = $('#value').val();
 		var keyExists = localStorage.getItem(key) !== null;
+		var file = '#' + key;
 		if (keyExists) {
 			removeEntry(key);
 			updateStatusLabel('key removed - ' + key);
+			$(file).remove();
 		} else if (key === '') {
 			updateStatusLabel('invalid input!')
 		} else {
@@ -232,12 +276,98 @@ $(document).ready(function () {
 
 });
 
+//recording interface
+function startRecording() {
+   console.log("recordButton clicked");   
+      // Simple constraints object, for more advanced audio features see
+      // https://addpipe.com/blog/audio-constraints-getusermedia/       
+    var constraints = { audio: true, video:false }
+      // Disable the record button until we get a success or fail from getUserMedia()
+   recordButton.disabled = true;
+   stopButton.disabled = false;
+   pauseButton.disabled = false  
+      // We're using the standard promise based getUserMedia() 
+      // https://developer.mozilla.org/en-US/docs/Web/API/MediaDevices/getUserMedia
+   navigator.mediaDevices.getUserMedia(constraints).then(function(stream) {
+      console.log("getUserMedia() success, stream created, initializing Recorder.js ...");
+         // create an audio context after getUserMedia is called
+         // sampleRate might change after getUserMedia is called, like it does on macOS when recording through AirPods
+         // the sampleRate defaults to the one set in your OS for your playback device
+      audioContext = new AudioContext();
+      //update the format 
+      /*  assign to gumStream for later use  */
+      gumStream = stream;     
+      /* use the stream */
+      input = audioContext.createMediaStreamSource(stream);
+         // Create the Recorder object and configure to record mono sound (1 channel)
+         // Recording 2 channels  will double the file size
+      rec = new Recorder(input,{numChannels:1})
+      //start the recording process
+      rec.record()
+      console.log("Recording started");
+   }).catch(function(err) {
+      //enable the record button if getUserMedia() fails
+      recordButton.disabled = false;
+      stopButton.disabled = true;
+      pauseButton.disabled = true
+   });
+}
 
+function pauseRecording(){
+   console.log("pauseButton clicked rec.recording=",rec.recording );
+   if (rec.recording){
+      //pause
+      rec.stop();
+      pauseButton.innerHTML="Resume";
+   }else{
+      //resume
+      rec.record()
+      pauseButton.innerHTML="Pause";
+   }
+}
+
+function stopRecording() {
+   console.log("stopButton clicked");
+   //disable the stop button, enable the record too allow for new recordings
+   stopButton.disabled = true;
+   recordButton.disabled = false;
+   pauseButton.disabled = true;
+   //reset button just in case the recording is stopped while paused
+   pauseButton.innerHTML="Pause";  
+   //tell the recorder to stop the recording
+   rec.stop();
+   //stop microphone access
+   gumStream.getAudioTracks()[0].stop();
+   //create the wav blob and pass it on to createDownloadLink
+   rec.exportWAV(createDownloadLink);
+}
+
+function createDownloadLink(blob) {   
+   var url = URL.createObjectURL(blob);
+   var au = document.createElement('audio');
+   var li = document.createElement('li');
+   var link = document.createElement('a');
+   //name of .wav file to use during upload and download (without extendion)
+   var filename = document.getElementById("key").value;
+   li.setAttribute('id', filename)
+   //add controls to the <audio> element
+   au.controls = true;
+   au.src = url;
+   //save to disk link
+   //link.href = url;
+   //link.download = filename+".wav"; //download forces the browser to donwload the file using the  filename
+   //link.innerHTML = "Save to disk";
+   //add the new audio element to li
+   li.appendChild(au);  
+   //add the filename to the li
+   li.appendChild(document.createTextNode(filename+".wav "))
+   //add the save to disk link to li
+   li.appendChild(link);
+   //add the li element to the ol
+   recordingsList.appendChild(li);
+}
 
 /*
-
-
-
 When an input element is given a name, that name becomes a property of the owning form element's HTMLFormElement.elements property. That means if you have an input whose name is set to guest and another whose name is hat-size, the following code can be used:
 
 let form = document.querySelector("form");
